f411027d8e0c9bbe57037a1e0c6f6cf3831d9bad
DEEZY1029 <ssobodai@st.ug.edu.gh>
2024-06-19 14:02:10 +0000

    Welcome

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
+import { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, ScrollView, Pressable, FlatList } from 'react-native';
 
-export default function App() {
+function LoginScreen({navigation}) {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    
+    
+      <ScrollView style={styles.ScrollView}>
+        <View style={styles.container}>
+      <View style={styles.jobizzContainer}>
+          <Image source={require('./assets/Jobizz.jpg')} />
+        </View>
+        <View style={styles.group46Container}>
+          <Image source={require('./assets/Group 46.jpg')} />
+        </View>
+        </View>
+        </ScrollView>
+        );
+     }
